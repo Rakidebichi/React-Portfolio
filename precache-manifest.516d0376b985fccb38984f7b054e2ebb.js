86308db2ac36b0c629e5614923ad363d79152bc5
rakidebichi <debbichiraki@outlook.com>
2024-01-05 21:10:17 +0100

    Updates

---
+++ precache-manifest.516d0376b985fccb38984f7b054e2ebb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd9627075951e08603e8",
+    "url": "/React-Portfolio/static/css/main.0aed2380.chunk.css"
+  },
+  {
+    "revision": "dd9627075951e08603e8",
+    "url": "/React-Portfolio/static/js/main.dd962707.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/React-Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "014a02721025d7ef3f736af55b33b219",
+    "url": "/React-Portfolio/index.html"
+  }
+];
